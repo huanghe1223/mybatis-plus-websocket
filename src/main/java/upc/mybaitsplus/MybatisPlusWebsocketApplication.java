4925315e8ea26bbe2f0c9ff0c95d4b9a6a4d3a9a
huanghe <h3145267275@163.com>
2024-07-27 15:02:18 +0800

    初始化

---
+++ src/main/java/upc/mybaitsplus/MybatisPlusWebsocketApplication.java
@@ -0,0 +1,13 @@
+package upc.mybaitsplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/upc/mybaitsplus/MybatisPlusWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.mybaitsplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
